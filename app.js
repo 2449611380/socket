49b8ebcb50fa1678d5d265a22156ec064771a34c
Yiwen <2449611380@qq.com>
2021-10-26 17:44:06 +0800

    大佬解决的问题QAQ

--- app.js
+++ app.js
@@ -5,32 +5,38 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
 
-app.use(express.static(__dirname + '/public/'))
+app.use(express.static(__dirname + '/public/'));
 
 app.get('/', (req, res) => {
     res.sendFile('index.html');
 });
 
-let Player_status = [];
-
-io.on('connection', (socket) => {
-    let key = '';
-    socket.on('disconnect', () => {
-        console.log('连接断开');
-        Player_status = Player_status.splice(key, key);
-    });
-
-    socket.on('player_data', data => {
-        Player_status.push(data);
-        key = Player_status.length - 1;
-
-        setInterval(() => {
-            socket.emit('AllPlayerStatusReturn', { key, Player_status });
-        }, 1000 / 120);
-    });
+server.listen(3000, err => {
+    console.log('server start : port * 3000');
 });
 
-server.listen(3000, err => {
-    if (err) throw err;
-    console.log('server start : port * 3000');
+let players = {};
+
+class Player {
+    constructor(id, name = '无名小卒', x, y) {
+        this.id = id;
+        this.name = name;
+        this.x = x;
+        this.y = y;
+    };
+};
+
+io.on('connection', (socket) => {
+    socket.emit('first_data', players);
+
+    socket.on('add_player', data => {
+        data.id = socket.id;
+        players[socket.id] = new Player(socket.id, data.name, data.x, data.y);
+        io.emit('add_player', players[socket.id]);
+    })
+
+    socket.on('disconnect', () => {
+        delete players[socket.id];
+        io.emit('leave_player', socket.id);
+    })
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,49 +1,41 @@
 let socket = io();
-let player_list = [];
 
-class Player {
-    constructor(name) {
-        this.name = name;
-        this.ranClass = `__${this.random(Number.MIN_SAFE_INTEGER, Number.MIN_SAFE_INTEGER)}`
-        this.x = this.random(0, 500);
-        this.y = 0;
-    };
+function addPlayerToDom(player) {
+    $('.app').append($(`<div class="player __${player.id}" style="left:${player.x}px !important;"></div>`))
+}
 
-    random(min, max) {
-        return ~~(Math.random() * max) + min;
-    };
+function randomNum(min, max) {
+    return ~~(Math.random() * max) + min;
 };
 
-$('button').on('click', () => {
-    if (!$('input').val()) return;
-    $('.mask').fadeOut(200);
-    socket.emit('player_data', new Player($('input').val()));
+let players = {};
+
+socket.emit('add_player', {
+    name: '无名小卒',
+    x: randomNum(0, 900),
+    y: 0
 });
 
-let Player_status_let = '';
-let l = 0;
+socket.on('first_data', data => {
+    Object.keys(data).map(key => {
+        addPlayerToDom(data[key]);
+    })
+})
 
+socket.on('add_player', data => {
+    addPlayerToDom(data);
+})
+
+socket.on('leave_player', id => {
+    $(`.__${id}`).remove();
+})
+
+
+let arrLength = 0;
 setInterval(() => {
-    socket.on('AllPlayerStatusReturn', data => {
-        Player_status_let = data;
+    if (arrLength >= 4) arrLength = 1;
+    arrLength++;
+    $('.player').css({
+        backgroundImage: `url(./images/Kid/Kid/Idle${arrLength}.png)`,
     });
-
-    // Player_status_let.Player_status[Player_status_let.key].x += l;
-}, 1000 / 120);
-
-document.onkeydown = (ev) => {
-    switch (ev.keyCode) {
-        case 37:
-            l = -1;
-
-            break;
-
-        case 39:
-            l = 1;
-            break;
-    }
-}
-
-document.onkeyup = (ev) => {
-    if (ev.keyCode == 37 || ev.keyCode == 39) return l = 0;
-}
+}, 1000 / 8);
